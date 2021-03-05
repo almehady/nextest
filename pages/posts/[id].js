e4850c1cecc396e7f45ab78eb63de1e2c7a400c8
almehady <almehady@gmail.com>
2021-03-05 21:25:21 +0300

    Feat: setup pages, routes etc.

---
+++ pages/posts/[id].js
@@ -0,0 +1,11 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
